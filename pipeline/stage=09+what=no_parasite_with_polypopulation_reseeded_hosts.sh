23c432d2ae754606ad788af7afd24246987d0d53
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-07-06 03:41:06 -0700

    Fix template paths for control job launch

--- pipeline/stage=09+what=no_parasite_with_polypopulation_reseeded_hosts.sh
+++ pipeline/stage=09+what=no_parasite_with_polypopulation_reseeded_hosts.sh
@@ -56,7 +56,7 @@ for replicate in $(seq "${NUM_REPS}"); do
 echo "replicate ${replicate}"
   SBATCH_SCRIPT_PATH="${SBATCH_SCRIPT_DIRECTORY_PATH}/$(uuidgen).slurm.sh"
   echo "SBATCH_SCRIPT_PATH ${SBATCH_SCRIPT_PATH}"
-  j2 --format=yaml -o "${SBATCH_SCRIPT_PATH}" "stage=07+what=evolve_parasite_with_polypopulation_reseeded_hosts/no_parasite_with_polypopulation_reseeded_hosts.slurm.sh.jinja" << J2_HEREDOC_EOF
+  j2 --format=yaml -o "${SBATCH_SCRIPT_PATH}" "stage=07+what=evolve_parasite_with_polypopulation_reseeded_hosts/evolve_parasite_with_polypopulation_reseeded_hosts.slurm.sh.jinja" << J2_HEREDOC_EOF
 inject_parasite_action_prepend: INJECT_PARASITE_ACTION_PREPEND='#'
 stage: '${STAGE}'
 what: no_parasite_with_polypopulation_reseeded_hosts
