9ba98fcf87d4cc0669918b8e025873be11f8ce5a
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-07-06 01:03:33 -0700

    Extract stage programatically

--- pipeline/stage=06+what=evolve_parasite_with_monopopulation_reseeded_hosts.sh
+++ pipeline/stage=06+what=evolve_parasite_with_monopopulation_reseeded_hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STAGE="06"
+export STAGE="$(echo "$0" | python3 -m keyname.cli extract stage)"
 echo "STAGE ${STAGE}"
 
 source "$(dirname "$0")/snippets/setup_instrumentation.sh"
