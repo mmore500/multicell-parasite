e1628b7792c80c7c35def8e1516f9e3baa5142c0
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-08-27 00:10:37 -0400

    Use Avida debug build in testing mode

--- pipeline/snippets/install_avida.sh
+++ pipeline/snippets/install_avida.sh
@@ -3,7 +3,7 @@
 export TMPDIR="${HOME}/scratch/tmp"
 mkdir -p "${TMPDIR}"
 
-AVIDA_REVISION="080669643ad4224cf49c5169e235ce3125e8dd05"
+AVIDA_REVISION="64f1b03ce30c5aa341e9704d4ff0f6109a375f1f"
 echo "AVIDA_REVISION ${AVIDA_REVISION}"
 
 while ! [ -L "${TMPDIR}/${AVIDA_REVISION}" ]; do
@@ -21,7 +21,11 @@ while ! [ -L "${TMPDIR}/${AVIDA_REVISION}" ]; do
     git checkout "${AVIDA_REVISION}"
     git submodule update --init --recursive
     git submodule foreach --recursive git status
-    ./build_avida
+    if [ "${RUNMODE}" == "TESTING" ]; then
+        ./build_avida -DCMAKE_BUILD_TYPE=Debug
+    else
+        ./build_avida
+    fi
   )
 
   ln -s "${AVIDA_INSTALL_PATH}" "${TMPDIR}/${AVIDA_REVISION}" || :
