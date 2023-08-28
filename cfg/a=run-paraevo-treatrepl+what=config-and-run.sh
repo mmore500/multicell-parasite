be732d4272e8f2efeb9238e230e62bcd25cb7929
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-08-28 16:03:31 -0400

    Try turning parasite intro smear off

--- cfg/a=run-paraevo-treatrepl+what=config-and-run.sh
+++ cfg/a=run-paraevo-treatrepl+what=config-and-run.sh
@@ -447,7 +447,9 @@ $(
   if [ "${EPOCH}" -eq 0 ]; then
     for pos in $(seq 0 4 "$(( WORLD_SIZE - 1 ))"); do
       smear_delay="$(( (NUM_UPDATES_INTRO_SMEAR * pos) / WORLD_SIZE  ))"
-      echo "u $(( 2000 + smear_delay )) InjectParasite parasite-smt.org ABB ${pos}"
+      # echo "u $(( 2000 + smear_delay )) InjectParasite parasite-smt.org ABB ${pos}"
+      # don't smear parasites
+      # echo "u 2000 InjectParasite parasite-smt.org ABB ${pos}"
     done
   fi
 )
