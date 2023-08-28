c6d1f4807339c5fc15b54e19ab2b932e767fa0e9
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-08-28 15:05:54 -0400

    Symlink latest joblogs for easy access

--- pipeline/snippets/setup_instrumentation.sh
+++ pipeline/snippets/setup_instrumentation.sh
@@ -13,6 +13,16 @@ echo "user ${USER}"
 ln -s "${HOME}/scratch" "/mnt/scratch/${USER}/" || :
 mkdir -p "${HOME}/joblog" || :
 
+if [[ -z ${SLURM_JOB_ID:-} ]]; then
+  export JOBLOG_PATH="/dev/null"
+else
+  export JOBLOG_PATH="${HOME}"/joblog/*id="${SLURM_JOB_ID}"+*
+fi
+echo "JOBLOG_PATH ${JOBLOG_PATH}"
+
+mkdir -p "${HOME}/joblatest" || :
+ln -s "${JOBLOG_PATH}" "${HOME}/joblatest/start" || :
+
 export JOBSCRIPT_PATH="$0"
 echo "JOBSCRIPT_PATH ${JOBSCRIPT_PATH}"
 readlink -f "${JOBSCRIPT_PATH}"
@@ -30,5 +40,14 @@ touch "${JOBCONT_PATH}"
 err() {
     echo "Error occurred:"
     awk 'NR>L-4 && NR<L+4 { printf "%-5d%3s%s\n",NR,(NR==L?">>>":""),$0 }' L="$1" "$0"
+    ln -s "${JOBLOG_PATH}" "${HOME}/joblatest/fail" || :
 }
 trap 'err $LINENO' ERR
+
+on_exit() {
+  if [[ $? -eq 0 ]]; then
+    ln -s "${JOBLOG_PATH}" "${HOME}/joblatest/succeed" || :
+  fi
+  ln -s "${JOBLOG_PATH}" "${HOME}/joblatest/finish" || :
+}
+trap on_exit EXIT
