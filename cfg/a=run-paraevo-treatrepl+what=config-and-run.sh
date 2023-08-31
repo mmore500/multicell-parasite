c0a5aa06686b90ff5a2fe8b237b067d2190d5074
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-08-31 16:42:39 -0400

    Set DEME_MIGRATION_RATE back to lower value

--- cfg/a=run-paraevo-treatrepl+what=config-and-run.sh
+++ cfg/a=run-paraevo-treatrepl+what=config-and-run.sh
@@ -206,7 +206,7 @@ DEMES_MIGRATION_METHOD 4  # necessary for parasite migration
 
 # Probability of a parasite migrating to a different deme
 # NOTE: only works with DEMES_MIGRATION_METHOD 4
-DEMES_PARASITE_MIGRATION_RATE 0.2
+DEMES_PARASITE_MIGRATION_RATE 0.002
 
 # Probability of an offspring being born in a different deme.
 DEMES_MIGRATION_RATE 0.0
