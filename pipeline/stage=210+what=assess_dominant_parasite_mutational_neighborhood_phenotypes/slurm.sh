aa77507522e554a1c2085bcc6cc7c332a2841fd1
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-09-28 13:33:09 -0400

    Bump source pin

--- pipeline/stage=210+what=assess_dominant_parasite_mutational_neighborhood_phenotypes/slurm.sh
+++ pipeline/stage=210+what=assess_dominant_parasite_mutational_neighborhood_phenotypes/slurm.sh
@@ -20,7 +20,7 @@ echo "SLURM_ARRAY_TASK_ID ${SLURM_ARRAY_TASK_ID}"
 
 SOURCE_URL="https://github.com/mmore500/multicell-parasite.git"
 echo "SOURCE_URL ${SOURCE_URL}"
-SOURCE_REVISION="d22740dd945e7c7459e2976c015e0d8ee2c567dd"
+SOURCE_REVISION="b307146920d1e8005c69a8aa4609fb795de14f63"
 echo "SOURCE_REVISION ${SOURCE_REVISION}"
 SCRIPT_PATH="pipeline/stage=210+what=assess_dominant_parasite_mutational_neighborhood_phenotypes.sh"
 echo "SCRIPT_PATH ${SCRIPT_PATH}"
