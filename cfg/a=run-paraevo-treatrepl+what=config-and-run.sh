1ea5ab2a2f08848319fe630132b08269447771b5
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-11-24 23:36:46 -0500

    Disable killdemehighestparasiteload

--- cfg/a=run-paraevo-treatrepl+what=config-and-run.sh
+++ cfg/a=run-paraevo-treatrepl+what=config-and-run.sh
@@ -478,7 +478,7 @@ $(
 
 $(
   if [ "${EPOCH_}" -ne 0 ]; then
-    echo "u 0:100 KillDemesHighestParasiteLoad 0.02"
+    # echo "u 0:100 KillDemesHighestParasiteLoad 0.02"
     echo "u 0:100 ReplicateDemesHighestBirthCount 0.03"
   fi
 )
