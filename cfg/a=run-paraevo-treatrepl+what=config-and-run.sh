b64d623ac1ee8447dcdf0af928343482af2bea36
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-11-25 15:35:19 -0500

    Decrease deme repl rate, make hosts do 3 reactions

--- cfg/a=run-paraevo-treatrepl+what=config-and-run.sh
+++ cfg/a=run-paraevo-treatrepl+what=config-and-run.sh
@@ -339,7 +339,7 @@ INJECT_METHOD 1
 INFECTION_MECHANISM 1
 
 # If set to 1, at least one reaction is required for a successful divide
-REQUIRE_SINGLE_REACTION 2
+REQUIRE_SINGLE_REACTION 3
 
 # Infection causes host steralization and takes all cpu cycles
 # (setting this to 1 will override inject_virulence)
@@ -482,7 +482,7 @@ $(
 $(
   if [ "${EPOCH_}" -ne 0 ]; then
     # echo "u 0:100 KillDemesHighestParasiteLoad 0.02"
-    echo "u 0:100 ReplicateDemesHighestBirthCount 0.03"
+    echo "u 0:100 ReplicateDemesHighestBirthCount 0.01"
   fi
 )
 
